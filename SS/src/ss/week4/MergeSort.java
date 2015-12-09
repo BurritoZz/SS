04bf89e8337317a0a2759fe5cacaa309f983bec9
BurritoZz <max-hendriks@mail.com>
2015-12-09 12:15:16 +0100

    Started on MergeSort

--- SS/src/ss/week4/MergeSort.java
+++ SS/src/ss/week4/MergeSort.java
@@ -4,7 +4,32 @@ import java.util.*;
 
 public class MergeSort {
     public static <Elem extends Comparable<Elem>>
-           void mergesort(List<Elem> list) {
-    	// TODO: implement, see exercise P-4.16
+    List<Elem> mergesort(List<Elem> list) {
+    	if (list.size() < 2) {
+    		return list;
+    	}
+    	List<Elem> fst = list.subList(0, list.size() / 2);
+    	List<Elem> snd = list.subList(list.size() / 2, list.size() - 1);
+    	fst = mergesort(fst);
+    	snd = mergesort(snd);
+    	List<Elem> res = new ArrayList<Elem>(); 
+    	int fi = 0;
+    	int si = 0;
+    	while (fi < fst.size() && si < snd.size()) {
+    		if (fst.get(fi).compareTo(snd.get(si)) < 0) {
+    			res.add(fst.get(fi));
+    			fi++;
+    		}
+    		else {
+    			res.add(snd.get(si));
+    			si++;
+    		}
+    	}
+    	if (fi <= fst.size() - 1) {
+    		res.addAll(fst.subList(fi, fst.size() - 1));
+    	} else if (si <= snd.size() - 1) {
+    		res.addAll(snd.subList(si, snd.size() - 1));
+    	}
+    	return res;
     }
 }
