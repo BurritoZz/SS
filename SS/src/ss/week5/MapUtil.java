bc854598c65e7441f02a322f6c9401e782a3fb29
BurritoZz <max-hendriks@mail.com>
2015-12-15 10:44:00 +0100

    Week 5 is AF!!!!!!!!

--- SS/src/ss/week5/MapUtil.java
+++ SS/src/ss/week5/MapUtil.java
@@ -11,8 +11,7 @@ public class MapUtil {
 	 * @param map
 	 * @return
 	 */
-	//@ requires map != null;
-	//@ ensures \result instanceOf boolean;
+    //@ ensures \result == (\forall K x1, x2; map.keySet().contains(x1) && map.keySet().contains(x2) && x1 != x2; map.get(x1) != map.get(x2));
     public static <K, V> boolean isOneOnOne(Map<K, V> map) {
     	Object[] keys = map.keySet().toArray(new Object[map.size()]);
     	Object[] values = new Object[map.size()];
@@ -28,8 +27,7 @@ public class MapUtil {
         return true;
     }
     
-    //@ requires map != null;
-    //@ requires range != null;
+    //@ ensures \result == (\forall V v1; range.contains(v1); \exists K k1; map.keySet().contains(k1); map.get(k1).equals(v1));
     public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
     	Object[] values = map.values().toArray(new Object[map.size()]);
     	Object[] rvalues = range.toArray(new Object[range.size()]);
@@ -45,6 +43,7 @@ public class MapUtil {
         return true;
     }
     
+    //@ ensures \forall K k1; map.keySet().contains(k1); \result.get(map.get(k)).contains(k1));
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
     	Map<V, Set<K>> result = new HashMap<V, Set<K>>();
     	for (K key : map.keySet()) {
@@ -57,6 +56,7 @@ public class MapUtil {
     	return result;
 	}
 
+    	//@ ensures \forall K k1; map.keySet().contains(k1); k1 == \result.get(map.get(k1));
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
 		Map<V, K> result = new HashMap<V, K>();
 		if (isOneOnOne(map)) {
@@ -67,12 +67,26 @@ public class MapUtil {
 		}
         return result;
 	}
+
+	//@ ensures (\forall V value; f.containsValue(value); g.containsKey(value)) == \result;
 	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.4
-        return false;
+        for (V value : f.values())	{
+        	if (!g.containsKey(value))	{
+        		return false;
+        	}
+        }
+        return true;
 	}
+
+	//@ ensures (\forall K k1; f.containsKey(k1); \result.get(k1).equals(g.get(f.get(k1))));
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.5
+	    if (compatible(f, g))	{
+        	HashMap<K, W> resultaat = new HashMap<K, W>();
+        	for (K key : f.keySet())	{
+        		resultaat.put(key, g.get(f.get(key)));
+        	}
+        	return resultaat;
+        }
         return null;
 	}
 }
---
+++ SS/src/ss/week6/Hello.java
@@ -0,0 +1,9 @@
+package ss.week6;
+
+import java.util.Scanner;
+
+public class Hello extends Scanner {
+
+    
+    
+}
